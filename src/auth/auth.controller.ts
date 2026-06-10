547a255acaf7ef8afe9d395cece3787df033994e
Laud Tetteh <kobbykurtx@gmail.com>
2026-06-10 22:56:50 +0000

    feat: add auth controller routes

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,71 @@
+import { Body, Controller, Get, Post, Res, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from '../user/dto/create-user.dto';
+import { LoginDto } from './dto/login.dto';
+import { CurrentUser } from './decorator/current-user.decorator';
+import { User } from '../user/entities/user.entity';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { GoogleAuthGuard } from './guards/google-auth.guard';
+import { type Response } from 'express';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  register(@Body() dto: CreateUserDto) {
+    return this.authService.register(dto);
+  }
+
+  @Post('login')
+  login(@Body() dto: LoginDto, @Res({ passthrough: true }) res: Response) {
+    return this.authService.login(dto, res);
+  }
+
+  @Post('refresh')
+  reIssueTokens(@Body('refreshToken') refreshToken: string) {
+    return this.authService.refreshTokens(refreshToken);
+  }
+
+  @UseGuards(JwtAuthGuard, GoogleAuthGuard)
+  @Get('profile')
+  getProfile(@CurrentUser() user: User) {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { id, ...rest } = user;
+
+    return rest;
+  }
+
+  // GOOGLE AUTHENTICATION
+  @Get('google/login')
+  @UseGuards(GoogleAuthGuard)
+  googleLogin() {}
+
+  @Get('google/callback')
+  @UseGuards(GoogleAuthGuard)
+  async googleCallback(
+    @CurrentUser() user: User,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const tokens = await this.authService.issueTokens(user);
+    this.authService.setAuthCookies(res, tokens);
+
+    return {
+      user,
+      message: 'Google login successful',
+    };
+  }
+
+  @Post('logout')
+  @UseGuards(JwtAuthGuard)
+  async logout(
+    @CurrentUser() user: User,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    await this.authService.clearCookiesAndTokens(user.id, res);
+
+    return {
+      message: 'Logged out successfully',
+    };
+  }
+}
