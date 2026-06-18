6d5f59dc41ba6792c3152cf8953521d984cd604d
Laud Tetteh <kobbykurtx@gmail.com>
2026-06-18 22:23:13 +0000

    feat: inject category as a dependency in products

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -3,9 +3,10 @@ import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
+import { CategoryModule } from '../category/category.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product])],
+  imports: [TypeOrmModule.forFeature([Product]), CategoryModule],
   controllers: [ProductsController],
   providers: [ProductsService],
 })
